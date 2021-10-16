593ee3aa92aee07547727237a6a41b168ee243fc
David Andrs <andrsd@gmail.com>
2021-10-16 06:22:07 -0600

    Using py2app for MacOS X deployment (again)

---
+++ Makefile
@@ -0,0 +1,36 @@
+CONDABASE=$(shell conda info --base)
+DIST=`pwd`/dist/
+FRAMEWORKS=$(DIST)/Concertista.app/Contents/Frameworks
+SRC=concertista
+
+all:
+	@echo ""
+
+create-venv:
+	@python -m venv venv
+
+init:
+	@pip install -e .
+	@pip install -r requirements/devel.txt
+
+syntax-check check-syntax:
+	@flake8 $(SRC) tests setup.py
+
+test:
+	@PYTEST_QT_API=pyqt5 pytest .
+
+coverage:
+	@PYTEST_QT_API=pyqt5 coverage run --source=$(SRC) -m pytest
+	@coverage html
+
+icon.icns:
+	@mkdir -p icon.iconset
+	@sips -z 512 512 $(SRC)/assets/icons/podcastista.png --out icon.iconset/icon_512x512.png
+	@iconutil -c icns icon.iconset
+	@rm -rf icon.iconset
+
+app:
+	@python setup.py py2app
+	@cp $(CONDABASE)/lib/libffi.7.dylib $(FRAMEWORKS)
+	@cp $(CONDABASE)/lib/libssl.1.1.dylib $(FRAMEWORKS)
+	@cp $(CONDABASE)/lib/libcrypto.1.1.dylib $(FRAMEWORKS)
--- setup.py
+++ setup.py
@@ -1,18 +1,71 @@
 from setuptools import setup
 from concertista import consts
+from glob import glob
+import platform
+import os
+
+main_script = 'concertista/__main__.py'
+assets_dir = 'concertista/assets'
+
+if platform.system() == 'Darwin':
+    PLIST_INFO = {
+        'CFBundleName': consts.APP_NAME,
+        'CFBundleDisplayName': consts.APP_NAME,
+        'CFBundleGetInfoString': consts.DESCRIPTION,
+        'CFBundleIdentifier': "name.andrs.osx.concertista",
+        'CFBundleVersion': str(consts.VERSION),
+        'CFBundleShortVersionString': str(consts.VERSION),
+        'NSHumanReadableCopyright': consts.COPYRIGHT
+    }
+
+    def generate_music_files(location):
+        data_files = []
+        for path, dirs, files in os.walk(os.path.join(location, 'music')):
+            install_dir = os.path.relpath(path, location)
+            ymls = [os.path.join(path, f) for f in files if f.endswith('.yml')]
+            data_files.append((install_dir, ymls))
+
+        return data_files
+
+    extra_options = dict(
+        setup_requires=['py2app'],
+        app=[main_script],
+        data_files=[
+            ('icons', glob(assets_dir + '/icons/*.svg')),
+            ('icons', glob(assets_dir + '/icons/*.png')),
+            ('', ['concertista/.env'])
+        ] + generate_music_files(assets_dir),
+        options={
+            'py2app': {
+                'argv_emulation': False,
+                'plist': PLIST_INFO,
+            }
+        }
+    )
+elif platform.system() == 'win32':
+    extra_options = dict(
+     setup_requires=['py2exe'],
+     app=[main_script],
+    )
+else:
+    extra_options = dict(
+        # Normally unix-like platforms will use "setup.py install" and install
+        # the main script as such
+        scripts=[main_script]
+    )
 
 setup(
     name='Concertista',
-    version='1.0',
+    version=consts.VERSION,
     author='David Andrš',
     author_email='andrsd@gmail.com',
     url='https://github.com/andrsd/concertista',
     license='LICENSE',
     description=consts.DESCRIPTION,
     install_requires=[
-        'PyQt5==5.15.2',
+        'PyQt5==5.13.1',
         'pyaml==20.4.0',
-        'spotipy==2.16.1',
+        'spotipy==2.19.0',
         'Flask==2.0.1',
         'waitress==1.4.4',
         'python-dotenv>=0.15.0'
@@ -30,4 +83,5 @@ setup(
     package_data={
         'concertista.assets': ['*.yml', '*.svg']
     },
+    **extra_options
 )
