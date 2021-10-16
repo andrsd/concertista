232846fe8684982b23fa95025cacc6b083a93899
David Andrs <andrsd@gmail.com>
2021-10-16 09:23:19 -0600

    Tests for preferences dialog

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import MagicMock
 
 
 @pytest.fixture
@@ -7,3 +8,11 @@ def main_window(qtbot):
     main = MainWindow()
     qtbot.addWidget(main)
     yield main
+
+
+@pytest.fixture
+def pref_dlg(qtbot, main_window):
+    from concertista.PreferencesWindow import PreferencesWindow
+    db = MagicMock()
+    dlg = PreferencesWindow(db, main_window)
+    yield dlg
