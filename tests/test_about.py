0e785844ea025ef507e112f02cef7f90833dce97
David Andrs <andrsd@gmail.com>
2021-10-16 07:01:35 -0600

    Tests for about dialog

--- tests/test_about.py
+++ tests/test_about.py
@@ -1,2 +1,10 @@
-def test_about_dialog(qtbot):
-    pass
+from PyQt5 import QtWidgets
+from concertista.AboutDialog import AboutDialog
+
+
+def test_about_dialog(main_window):
+    about_dlg = AboutDialog(main_window)
+
+    assert isinstance(about_dlg, AboutDialog)
+    assert about_dlg.windowTitle() == 'About'
+    assert isinstance(about_dlg._layout, QtWidgets.QVBoxLayout)
